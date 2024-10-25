35b86868a3ea9758f2c7e30f9cb6fa7d8819d5c1
anastasia.peretyatko <aperetyatko989@gmail.com>
2024-10-25 21:03:26 +0300

    TSK-5: connect i18n lib

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+module.exports = {
+  i18n: {
+    locales: ['en', 'ru'],
+    defaultLocale: 'ru'
+  }
 };
-
-export default nextConfig;
