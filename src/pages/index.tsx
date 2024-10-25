0751c058eb1eaec961ae6aaa2ee4120496ea7c35
anastasia.peretyatko <aperetyatko989@gmail.com>
2024-10-26 00:58:25 +0300

    TSK-6: add style chakra ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
-import {GetStaticPropsContext} from 'next';
-import {useTranslations} from 'next-intl';
+import { ColorModeButton } from '@/components/ui/color-mode';
+import { Box, Text } from '@chakra-ui/react';
+import { GetStaticPropsContext } from 'next';
+import { useTranslations } from 'next-intl';
 
 export default function Index() {
   const t = useTranslations();
 
   return (
-    // <PageLayout title={t('title')}>
-      <p>{t('Hello')}</p>
-    // </PageLayout>
+    <> </>
   );
 }
 
-export async function getStaticProps({locale}: GetStaticPropsContext) {
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
   return {
     props: {
       messages: (await import(`../../messages/${locale}.json`)).default
