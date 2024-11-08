306ff4ff7aba25c464cbd1e77bb5840633f3d48a
anastasia.peretyatko <aperetyatko989@gmail.com>
2024-11-08 10:33:54 +0300

    TSK-8: вынесенна в отдельную переменную массив языков

---
+++ src/common/assets/language.ts
@@ -0,0 +1,15 @@
+export type SelectArrayType = {
+  title: string
+  value: string
+}
+
+export const LANGUAGES: SelectArrayType[] = [
+  {
+    title: 'English',
+    value: 'en',
+  },
+  {
+    title: 'Russian',
+    value: 'ru',
+  },
+]
