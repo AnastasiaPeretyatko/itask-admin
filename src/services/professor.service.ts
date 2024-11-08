4e8dfe9c3b8e3cd7fc77086eeb3f35fe865e6a66
anastasia.peretyatko <aperetyatko989@gmail.com>
2024-11-08 10:39:01 +0300

    TSK-8: добавление эндпоинтов для преподавателей

--- src/services/professor.service.ts
+++ src/services/professor.service.ts
@@ -1,8 +1,17 @@
-import { TProfessorCreate } from '@/types/professor'
+import { TProfessor, TProfessorCreate } from '@/types/professor'
 import { http } from '.'
 import { TParams } from '@/store/professors/professors.slice'
 
 export const postProfessorRequest = (data: TProfessorCreate) =>
   http.post('/professors', data)
 
-export const getProfessorsRequest = (params: TParams) => http.get('/professors', {params})
+export const getProfessorsRequest = (params: TParams) =>
+  http.get('/professors', { params })
+
+export const patchProfessorRequest = ({
+  id,
+  data,
+}: {
+  id: string
+  data: Partial<TProfessor>
+}) => http.patch(`/professors/${id}`, data)
