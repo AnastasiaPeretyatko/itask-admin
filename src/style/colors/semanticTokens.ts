9dd9a6490832e8ee4fd6f41ff0ba607fa81ed1a5
anastasia.peretyatko <aperetyatko989@gmail.com>
2025-02-20 22:47:25 +0300

    TSK-58: вынесение цвета в отдельный файл

---
+++ src/style/colors/semanticTokens.ts
@@ -0,0 +1,29 @@
+export const semanticTokens = {
+  colors: {
+    font: { _dark: 'whiteAlpha.900', _light: 'blackAlpha.800' },
+    background: {
+      fill: { _dark: '#1A1D1F', _light: 'white.100' },
+      main: { _dark: 'black.300', _light: 'white.300' },
+      switch: { _dark: 'black.100', _light: 'white.300' },
+      button: { _dark: 'whiteAlpha.200', _light: 'neutral.light' },
+      natural: { _dark: 'black.100', _light: '#e2e3e72e' },
+    },
+    bg: { _dark: 'black', _light: 'white.300' },
+    input: {
+      outline: { _dark: 'whiteAlpha.200', _light: 'blackAlpha.300' },
+    },
+    selector: {
+      bg: { _dark: '#2e3337', _light: 'white.100' },
+    },
+    notify: {
+      bg: { _dark: 'primary.darkBlue', _light: 'white.100' },
+    },
+    text: {
+      bold: { _dark: 'whiteAlpha.800', _light: 'blackAlpha.800' },
+      pale: { _dark: 'whiteAlpha.600', _light: 'blackAlpha.600' },
+    },
+    button: {
+      bg: { _dark: 'whiteAlpha.900', _light: 'blackAlpha.400' },
+    },
+  },
+};
