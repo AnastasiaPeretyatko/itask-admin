65473b9ecec8f4622d330cf94e2a7ed4641b7550
anastasia.peretyatko <aperetyatko989@gmail.com>
2025-02-24 11:40:47 +0300

    вынести цвета в отдельный файл

--- src/style/colors/colors.ts
+++ src/style/colors/colors.ts
@@ -14,15 +14,12 @@ export const colors = {
   },
 
   black: {
+    10: 'rgba(0, 0, 0, 0.1)',
+    50: 'rgba(0, 0, 0, 0.5)',
     100: '#111315',
     200: '#1A1D1F',
     300: '#272B30',
     400: '#303336',
-  },
-  white: {
-    100: '#ffffff',
-    200: '#FCFCFC',
-    300: '#F4F4F4',
   },
   red: {
     100: '#FFECEE',
@@ -32,4 +29,26 @@ export const colors = {
     medium: '#B0B0B0',
     dark: '#303336',
   },
+
+  almostBlack: '#111319',
+  almostBlack20: 'rgba(17, 19, 25, 0.8)',
+  almostWhite: '#e6e6e6',
+
+  white: {
+    primary: '#fff',
+    5: 'rgba(255, 255, 255, 0.05)',
+    10: 'rgba(255, 255, 255, 0.1)',
+    20: 'rgba(255, 255, 255, 0.2)',
+    25: 'rgba(255, 255, 255, 0.25)',
+    50: 'rgba(255, 255, 255, 0.5)',
+    75: 'rgba(255, 255, 255, 0.75)',
+  },
+  warmGrey: '#f2f2f2',
+  veryDarkBlue: '#08090c',
+  slate: {
+    primary: '#9ba6b2',
+    dark: '#394351',
+    light: '#dae1e9',
+  },
+  lightBlack: '#2f3336',
 };
--- src/style/colors/semanticTokens.ts
+++ src/style/colors/semanticTokens.ts
@@ -3,10 +3,16 @@ export const semanticTokens = {
     font: { _dark: 'whiteAlpha.900', _light: 'blackAlpha.800' },
     background: {
       fill: { _dark: '#1A1D1F', _light: 'white.100' },
-      main: { _dark: 'black.300', _light: 'white.300' },
+      // main: { _dark: 'black.300', _light: 'white.300' },
       switch: { _dark: 'black.100', _light: 'white.300' },
       button: { _dark: 'whiteAlpha.200', _light: 'neutral.light' },
       natural: { _dark: 'black.100', _light: '#e2e3e72e' },
+
+      //? new
+      main: { _dark: 'almostBlack', _light: 'white.primary' },
+      20: { _dark: 'almostBlack20', _light: 'white.20' },
+      secondary: { _dark: 'black.50', _light: 'warmGrey' },
+
     },
     bg: { _dark: 'black', _light: 'white.300' },
     input: {
@@ -18,12 +24,44 @@ export const semanticTokens = {
     notify: {
       bg: { _dark: 'primary.darkBlue', _light: 'white.100' },
     },
+
+
+
+    link: { _light: 'primary.blue', _dark: 'primary.darkBlue' },
     text: {
-      bold: { _dark: 'whiteAlpha.800', _light: 'blackAlpha.800' },
-      pale: { _dark: 'whiteAlpha.600', _light: 'blackAlpha.600' },
+      primary: { _light: 'almostBlack', _dark: 'almostWhite' },
+      lighter: { _light: '#808aa9', _dark: '#fff' },
+      secondary: { _light: 'slate.dark', _dark: '#b8c0c8' },
+      tertiary: { _light: 'slate.primary', _dark: 'slate.primary' },
     },
+    placeholder: { _light: '#a2b2c3', _dark: 'slate.dark' },
+    sidebar: {
+      bg: { _light: 'warmGrey', _dark: 'veryDarkBlue' },
+      text: { _light: 'rgb(78, 92, 110)', _dark: 'slate' },
+      hoverButton: { _dark: 'white.50', _light: 'almostBlack' },
+      hoverText: { _light: 'white.75', _dark: 'rgb(78, 92, 110)' },
+    },
+    shadow: { _light: 'rgba(0, 0, 0, 0.2)', _dark: 'rgba(0, 0, 0, 0.6)' },
+    modal: {
+      backdrop: { _light: 'black.10', _dark: 'black.50' },
+      bg: { _light: 'white.primary', _dark: '#1f2128' },
+    },
+    divider: { _light: 'slate.light', _dark: '#262a37' },
     button: {
-      bg: { _dark: 'whiteAlpha.900', _light: 'blackAlpha.400' },
+      neuteal: {
+        bg:{ _light: 'white.primary', _dark: 'almostBlack' },
+        bgDarker05: { _light: '#f2f2f2', _dark: '#07070a' },
+        text: { _light: 'almostBlack', _dark: 'white.primary' },
+        border: { _light: '#d9d9d9', _dark: 'slate.dark' },
+      },
+    },
+    tooltip: {
+      bg: { _light: 'almostBlack', _dark: 'white.primary' },
+      text: { _light: 'white.primary', _dark: 'lightBlack' },
+    },
+    toast: {
+      bg: { _light: 'almostBlack', _dark: 'white.primary' },
+      text: { _light: 'white.primary', _dark: 'lightBlack' },
     },
   },
 };
